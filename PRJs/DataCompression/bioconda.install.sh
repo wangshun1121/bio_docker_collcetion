efc1e7506e19eb7ca1ed9c1ca02554a886d5675d
wangshun1121 <421309531@qq.com>
2020-01-06 19:58:52 +0800

    Update bioconda.install.sh

--- PRJs/DataCompression/bioconda.install.sh
+++ PRJs/DataCompression/bioconda.install.sh
@@ -1,5 +1,5 @@
 wget -c https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh -O Miniconda2.sh
-bash ./Miniconda2.sh -b
+bash ./Miniconda2.sh -b -p /opt/miniconda2
 /root/miniconda2/bin/conda config --add channels defaults
 /root/miniconda2/bin/conda config --add channels bioconda
 /root/miniconda2/bin/conda config --add channels conda-forge
